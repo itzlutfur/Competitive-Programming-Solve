0f4be689540cc5dd63ce34e39a7d6e063f7dd03f
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-05-31 21:04:56 +0600

    1037 interval

--- Practice.cpp
+++ Practice.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
-#include <math.h>
 using namespace std;
 
 int main() {
 
-    double A, B, C;
-    cin >> A >> B >> C;
-    cout << fixed << setprecision(5);
-    double temp = B*B-4*A*C;
-
-    if( temp<0 || A==0) {
-        cout << "Impossivel calcular" << endl;
-    } else {
-        double r1 = (-B+sqrt(temp))/(2*A);
-        double r2 = (-B-sqrt(temp))/(2*A);
-        cout << "R1 = " << r1 << "\n" << "R2 = " << r2 << endl;
+    float A;
+    cin >> A;
+    
+    if(A<0.00000){
+        cout << "Fora de intervalo" << endl;
+    }else if(A>0.00000 && A<=25){
+        cout << "Intervalo [0,25]" << endl;
+    }else if(A>25.00000 && A<=50){
+        cout << "Intervalo (25,50]" << endl;
+    }else if(A>50.00000 && A<=75){
+        cout << "Intervalo (50,75]" << endl;
+    }else if(A>=75.00000 && A<=100){
+        cout << "Intervalo (75,100]" << endl;
+    }else if(A > 100.0000) {
+        cout << "Fora de intervalo\n";
     }
-
     return 0;
 }
