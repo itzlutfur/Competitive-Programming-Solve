5dcfb82f5d44fb89844f53ec71512d4c7626c38e
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-05-31 21:12:27 +0600

    1040 Average 3

--- Practice.cpp
+++ Practice.cpp
@@ -1,17 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    if(t<=1399){
-        cout << "Division 4" << endl;
-    } else if(t>1399 && t<=1599) {
-        cout << "Division 3" << endl;
-    }else if(t>=1600 && t<=1899){
-        cout << "Division 2" << endl;
-    } else if(t>=1900) {
-        cout << "Division 1" << endl;
+    double N1, N2, N3, N4, last, avg;
+    cin >> N1 >> N2 >> N3 >> N4;
+
+    avg = (N1*2+N2*3+N3*4+N4)/10;
+
+    cout << fixed << setprecision(1);
+    cout << "Media: " << avg << endl;
+
+    if(avg>=7.0){
+    cout << "Aluno aprovado." << endl;
+
+    } else if(avg<5.0){
+        cout << "Aluno reprovado." << endl;
+
+    } else if(avg>=5.0 && avg<=6.9){
+        cout << "Aluno em exame." << endl;
+        cin >> last;
+        cout << "Nota do exame: " << last << endl;
+        avg = (avg + last)/2;
+
+        if(avg >=5.0) {
+            cout << "Aluno aprovado." << endl;
+        } else {
+            cout << "Aluno reprovado." << endl;
+        }
+        cout << "Media final: " << avg << endl;
     }
 
     return 0;
