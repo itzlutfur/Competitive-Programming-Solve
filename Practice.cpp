ade848ca2d183c5fff837ff210bc2818f241ffc0
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-05-31 21:03:13 +0600

    Problem 1036

--- Practice.cpp
+++ Practice.cpp
@@ -1,9 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <math.h>
 using namespace std;
 
 int main() {
 
-    cout << "Hello World" << endl;
-    
+    double A, B, C;
+    cin >> A >> B >> C;
+    cout << fixed << setprecision(5);
+    double temp = B*B-4*A*C;
+
+    if( temp<0 || A==0) {
+        cout << "Impossivel calcular" << endl;
+    } else {
+        double r1 = (-B+sqrt(temp))/(2*A);
+        double r2 = (-B-sqrt(temp))/(2*A);
+        cout << "R1 = " << r1 << "\n" << "R2 = " << r2 << endl;
+    }
+
     return 0;
 }
