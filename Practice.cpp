c00978b15097ddb507cd0b6587375225dadc5699
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-05-31 21:15:48 +0600

    1045 Triangle Types

--- Practice.cpp
+++ Practice.cpp
@@ -3,14 +3,42 @@ using namespace std;
 
 int main() {
 
-    string test;
-    cin >> test;
+    double x, y, z, a, b, c, A, B, C;
+    cin >> x >> y >> z;
 
-    cout << test[3] << test[4] << "/" << test[0] << test[1] << "/" << test[6] << test[7]<<endl;
-    cout << test[6] << test[7] << "/" << test[3] << test[4] << "/" << test[0] << test[1]<<endl;
-    cout << test[0] << test[1] << "-" << test[3] << test[4] << "-" << test[6] << test[7]<<endl;
+    if(x>y && x>z){
+        a=x; b=y; c=z;
+    } else if(y>z) {
+        a=y; b=x; c=z;
+    } else {
+        a=z; b=x; c=y;
+    }
 
+    A = a*a;
+    B = b*b;
+    C = c*c;
 
+    if(a>=(b+c)){
+        cout << "NAO FORMA TRIANGULO" << endl;
+    }
+    else if(A==(B+C)){
+        cout << "TRIANGULO RETANGULO" << endl;
+    } 
+    else if(A>(B+C)) {
+        cout << "TRIANGULO OBTUSANGULO" << endl;
+    }
+    else if(A<(B+C)) {
+        cout << "TRIANGULO ACUTANGULO" << endl;
+    }
+
+    {
+    if(a==b && b==c && c==a) {
+        cout << "TRIANGULO EQUILATERO" << endl;
+    }
+    else if(a==b || b==c || c==a) {
+        cout << "TRIANGULO ISOSCELES" << endl;
+    }
+    }
 
     return 0;
 }
