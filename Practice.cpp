e537bf876b119b86ded5f3e4670c3492526a8cb7
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-06-13 23:24:43 +0600

    1071 Sum of Consecutive Odd Numbers i

--- Practice.cpp
+++ Practice.cpp
@@ -1,44 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
+    int max, min, X, Y;
+    int sum = 0;
 
-    double x, y, z, a, b, c, A, B, C;
-    cin >> x >> y >> z;
+    cin >> X >> Y;
 
-    if(x>y && x>z){
-        a=x; b=y; c=z;
-    } else if(y>z) {
-        a=y; b=x; c=z;
-    } else {
-        a=z; b=x; c=y;
-    }
+    max = X>Y ? X : Y;
+    min = max==X ? Y: X;
 
-    A = a*a;
-    B = b*b;
-    C = c*c;
-
-    if(a>=(b+c)){
-        cout << "NAO FORMA TRIANGULO" << endl;
-    }
-    else if(A==(B+C)){
-        cout << "TRIANGULO RETANGULO" << endl;
-    } 
-    else if(A>(B+C)) {
-        cout << "TRIANGULO OBTUSANGULO" << endl;
-    }
-    else if(A<(B+C)) {
-        cout << "TRIANGULO ACUTANGULO" << endl;
+    for(int i = min+1; i < max; i++){
+        if(i%2!=0){
+            sum += i;
+        }
     }
 
-    {
-    if(a==b && b==c && c==a) {
-        cout << "TRIANGULO EQUILATERO" << endl;
-    }
-    else if(a==b || b==c || c==a) {
-        cout << "TRIANGULO ISOSCELES" << endl;
-    }
-    }
+    cout << sum << endl; 
 
     return 0;
 }
