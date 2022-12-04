01eccfe2bf2c101efc3c4349beaac202ddcfd8fc
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-12-04 22:51:41 +0600

    Event

---
+++ 2172.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, m, ans;
+
+    while (cin >> x >> m && x != 0 && m != 0)
+    {
+        ans = m * x;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
