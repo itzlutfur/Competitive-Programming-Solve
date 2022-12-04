92553a1e61185cc2eb7c0df0042117d908b02d17
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-12-05 01:24:37 +0600

    Bob Conduit

---
+++ 1589.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, r1, r2, ans;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> r1 >> r2;
+        ans = r1 + r2;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
