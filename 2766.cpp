29005d5007987d3a55eb73a1f9bd8187093d34c2
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-12-04 16:12:26 +0600

    Input and Output Reading and Skipping Names

---
+++ 2766.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  string str;
+  for (int i = 0; i < 10; i++)
+  {
+
+    cin >> str;
+
+    if (i == 2)
+    {
+      cout << str << endl;
+    }
+    else if (i == 6)
+    {
+      cout << str << endl;
+    }
+    else if (i == 8)
+    {
+      cout << str << endl;
+    }
+  }
+
+  return 0;
+}
