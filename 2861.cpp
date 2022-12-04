00993b32fe1ce54fd39021bb6569de2e7d6acfe3
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-12-05 01:05:24 +0600

    The Output

---
+++ 2861.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    string question;
+
+    cin >> c;
+    for (int i = 0; i < c; i++)
+    {
+        cin >> question;
+        getline(cin, question);
+        cout << "gzuz" << endl;
+    }
+
+    return 0;
+}
