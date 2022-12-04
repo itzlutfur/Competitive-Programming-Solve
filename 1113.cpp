364f6ed099613858ec06c1d4801e553ab83e1593
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2022-12-04 22:39:01 +0600

    Ascending and Descending

---
+++ 1113.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    while (true)
+    {
+        cin >> x >> y;
+
+        if (x == y)
+        {
+            break;
+        }
+
+        if (x > y)
+        {
+            cout << "Decrescente" << endl;
+        }
+        else if (x < y)
+        {
+            cout << "Crescente" << endl;
+        }
+    }
+}
