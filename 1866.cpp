b5aef0d8fbe500cb49f5590d044b254eb52ef8cc
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2023-08-17 19:49:44 +0600

    Bill

---
+++ 1866.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    cin >> c;
+
+    while (c--)
+    {
+        int n;
+        cin >> n;
+
+        if (n % 2 == 0)
+        {
+            cout << 0 << "\n";
+        }
+        else
+        {
+            cout << 1 << "\n";
+        }
+    }
+}
